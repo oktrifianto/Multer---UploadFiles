2cfa8790204d0df74317123d6c7960882bf0e5e0
Rahmat <oktrifianto@gmail.com>
2022-06-26 10:18:02 +0700

    Add new file `server.js` is succesful

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.get("/", (req, res) => {
+  res.send({
+    message : "Hi from back-end server!",
+    error   : []
+  });
+});
+
+app.listen(3000, () => console.log(`Server running!`));
