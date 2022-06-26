599ea84da07f59455d1a1101ed33be3324a5eb62
Rahmat <oktrifianto@gmail.com>
2022-06-26 10:29:11 +0700

    Update form `main.js` in the front-end

--- front-end/main.js
+++ front-end/main.js
@@ -1,1 +1,24 @@
-console.log('hi there from console');
+const server = 'http://localhost:3000';
+const f = document.getElementById("form");
+f.addEventListener("submit", submitData);
+
+function submitData(e){
+	e.preventDefault();
+	const name 	= document.getElementById("name");
+	const files = document.getElementById("files");	
+	const formData = new FormData();
+	formData.append("name", name.value);
+	for (let i=0; i < files.files.length; i++){
+		formData.append("files", files.files[i]);
+	}
+
+	fetch(`${server}/upload`, {
+		method: 'POST',
+		body: formData,
+		headers: {
+			"Content-Type": "multipart/form-data"
+		}
+	})
+	.then((res) => console.log(res))
+	.catch((err) => ("Something error", err));
+}
