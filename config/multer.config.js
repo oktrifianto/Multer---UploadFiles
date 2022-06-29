810c19bbf49a2e45285cd98e9d2cd6d51b0b3054
Rahmat <oktrifianto@gmail.com>
2022-06-29 16:35:46 +0700

    Fix: add clean string image

--- config/multer.config.js
+++ config/multer.config.js
@@ -4,13 +4,14 @@
  */
 const multer      = require('multer');
 const date        = require('../services/DateForImageName');
+const cleanImage  = require('../services/CleanImageName');
 
 const MyStorage = multer.diskStorage({
   destination: function(req, file, callback) {
     callback(null, 'uploads/');
   },
   filename: function(req, file, callback){
-    callback(null, date + '---' + req.body.name + '.jpg');
+    callback(null, date + '---' + cleanImage(req.body.name) + '.jpg');
   }
 });
 
