1f2f24dd9d206051924b53f369cfee52c108638b
Rahmat <oktrifianto@gmail.com>
2022-06-29 11:32:22 +0700

    Fixed: date for image name

--- services/DateForImageName.js
+++ services/DateForImageName.js
@@ -2,5 +2,16 @@
  * Format YYYYMMDD
  * ---------------
  * Example : 20220602
+ * 
+ * @see https://stackoverflow.com/questions/12409299/how-to-get-current-formatted-date-dd-mm-yyyy-in-javascript-and-append-it-to-an-i
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toJSON?retiredLocale=id
  */
-module.exports = `${new Date().getFullYear()}0${new Date().getMonth() + 1}0${new Date().getDay()}`;
+const time = new Date();
+let yyyy = time.getFullYear();
+let mm   = time.getMonth() + 1;
+let dd   = time.getDate();
+
+if (mm < 10) mm = '0' + mm;
+if (dd < 10) dd = '0' + dd;
+
+module.exports = yyyy + '' + mm + '' + dd;
